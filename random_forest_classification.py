8df132918d75749dbed82876135cb810af996294
prajwal9404 <prajwal9404@gmail.com>
2023-02-05 11:36:32 +0530

    first commit

--- random_forest_classification.py
+++ random_forest_classification.py
@@ -4,7 +4,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
+import pickle 
 # Importing the dataset
 dataset = pd.read_csv('Social_Network_Ads.csv')
 X = dataset.iloc[:, :-1].values
